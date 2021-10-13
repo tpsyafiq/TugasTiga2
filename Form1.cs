264ebf91f6779df3fc8e78739c01e67e4e50491c
tpsyafiq <thoha.syafiq03@gmail.com>
2021-10-13 20:43:57 +0700

    pemrograman lanjutan

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasTiga2
+{
+    public partial class frmTugasTiga2 : Form
+    {
+        public frmTugasTiga2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            var nama = txtNama.Text;
+            var pendidikan = cmbPendidikan.Text;
+            var pekerjaan = lstPekerjaan.Text;
+
+            txtPesan1.Text = string.Format("Halo {0}", nama);
+            txtPesan2.Text = string.Format("Pendidikan anda {0}", pendidikan);
+            txtPesan3.Text = string.Format("Anda seorang {0}, hebat", pekerjaan);
+        }
+    }
+}
